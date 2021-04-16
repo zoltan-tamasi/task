95f270dedf4e135d53f768b4e372b4ae32806669
Zoltan Tamasi <mail@zoltantamasi.net>
2021-04-16 12:46:51 +0200

    Utilize etag header for cacheing.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
 
-const API_URL = 'https://partners.betvictor.mobi/en-gb/in-play/1/events';
+const API_HOST = 'partners.betvictor.mobi';
+const API_PATH = '/en-gb/in-play/1/events';
 
 const app = express();
 const port = 3000;
 
-const eventService = require('./service/event-service')(API_URL);
+const eventService = require('./service/event-service')({ host: API_HOST, path: API_PATH });
 const { getSportsController } = require('./controller/controller')(eventService);
 
 app.get('/sports', getSportsController);
--- service/event-service.js
+++ service/event-service.js
@@ -2,16 +2,32 @@
 const https = require('https');
 
 const sports = new Map();
+let eTag;
 
-const getData = (apiUrl) => () => 
+const getData = ({ host, path }) => () => 
   new Promise((resolve, reject) => {
-    https.get(apiUrl, response => {
+    https.get({
+      protocol: 'https:',
+      headers: eTag ? {
+        'If-None-Match': eTag,
+      } : {},
+      host,
+      path
+    }, response => {
+      eTag = response.headers.etag;
       let data = '';
       response.on('data', (chunk) => {
         data += chunk;
       });
       response.on('end', () => {
-        resolve(JSON.parse(data));
+        if (response.statusCode === 200) {
+          resolve(JSON.parse(data));
+          console.log('New data read');
+        } else if (response.statusCode === 304) {
+          console.log('No new data');
+        } else {
+          reject(data);
+        }
       });
     }).on("error", (err) => {
       reject(err);
@@ -20,14 +36,13 @@ const getData = (apiUrl) => () =>
     response.result.sports.forEach(sport => {
       sports.set(sport.id, sport);
     });
-    console.log('Data read');
   });
 
 const getSports = () => 
   Array.from(sports.values()).map(({ desc }) => desc);
 
-module.exports = (apiUrl) => {
-  setInterval(getData(apiUrl), 5000);
+module.exports = ({ host, path }) => {
+  setInterval(getData({ host, path }), 5000);
 
   return {
     getSports,
